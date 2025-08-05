483e67c0fca395cda2c1fcff6379c8359aa066a9
Ratnesh-Dhan <ratneshdhan@gmail.com>
2025-08-05 18:48:43 +0530

    frontend fixxed .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   const [loading, setLoading] = useState<boolean>(false);
 
   const handleAsk = async () => {
+    setAnswer("");
     try {
       setLoading(true);
       console.log("question: ", question);
@@ -23,26 +24,14 @@ export default function Home() {
         res.body.getReader();
       const decoder: TextDecoder = new TextDecoder("utf-8");
 
-      let buffer = "";
+      // let buffer = "";
 
       while (true) {
         const { done, value } = await reader.read();
         if (done) break;
 
-        buffer += decoder.decode(value, { stream: true });
-        // const chunk = decoder.decode(value, { stream: true });
-        const messages = buffer.split("\n\n");
-        for (let i = 0; i < messages.length - 1; i++) {
-          const msg = messages[i].trim();
-          if (msg.startsWith("data:")) {
-            const content = msg.replace(/^data:\s*/, "");
-            if (content === "[DONE]") return;
-
-            // Append to answer like ChatGPT style
-            setAnswer((prev) => prev + content);
-          }
-        }
-        // setAnswer((prev) => prev + chunk);
+        const chunk = decoder.decode(value, { stream: true });
+        setAnswer((prev) => prev + chunk);
       }
     } catch (err) {
       if (err instanceof Error) {
@@ -62,7 +51,7 @@ export default function Home() {
       <textarea
         value={question}
         onChange={(e) => setQuestion(e.target.value)}
-        className="w-full max-w-xl p-4 border rounded-lg mb-4 h-32 text-black"
+        className=" w-2/3 p-4 border rounded-lg mb-4 h-32 text-black"
         placeholder="Ask a question based on your docs..."
       />
       <button
@@ -74,9 +63,9 @@ export default function Home() {
       </button>
 
       {answer && (
-        <div className="mt-6 w-full max-w-xl bg-white p-4 rounded shadow">
+        <div className="mt-6 w-2/3 bg-white p-4 rounded shadow">
           <h2 className="text-lg font-semibold mb-2 text-black">Answer:</h2>
-          <p className="text-black">{answer}</p>
+          <pre className="text-black whitespace-pre-wrap">{answer}</pre>
         </div>
       )}
     </div>
